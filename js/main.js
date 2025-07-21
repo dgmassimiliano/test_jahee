c7ad6a3a3311bac3b9a3543da8555e2d27741c1c
dgmassimiliano <76176709+dgmassimiliano@users.noreply.github.com>
2025-07-21 12:31:27 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const banner = document.getElementById("cookie-banner");
   const acceptBtn = document.getElementById("accept-cookies");
   const navLinks = document.querySelectorAll('.nav-link');
-  const currentURL = window.location.pathname.replace(/\/$/, '');
+  const currentURL = window.location.pathname;
 
   navLinks.forEach(link => {
-    const linkURL = new URL(link.href).pathname.replace(/\/$/, '');
+    const linkURL = new URL(link.href).pathname;
     if (currentURL === linkURL) {
       link.classList.add('active');
     }
@@ -23,9 +23,9 @@ document.addEventListener("DOMContentLoaded", function () {
     localStorage.setItem("cookies-accepted", "true");
     banner.style.display = "none";
   });
-  alert(currentURL);
+
   //Photo Gallery
-  if(currentURL.endsWith("/photo-gallery")){  
+  if(currentURL.endsWith("/photo-gallery/")){  
     const modal = new bootstrap.Modal(document.getElementById('imageModal'));
     const modalImage = document.getElementById('modalImage');
     const thumbs = document.querySelectorAll('.gallery-thumb');
@@ -41,5 +41,4 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   }
-
 });
